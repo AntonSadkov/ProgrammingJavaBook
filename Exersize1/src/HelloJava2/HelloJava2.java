e6a68c4537774809032bca84fd5490a26fc2b390
antonsadkov <antonsadkov96@gmail.com>
2023-07-13 17:15:54 +0300

    Add small changes

--- Exersize1/src/HelloJava2/HelloJava2.java
+++ Exersize1/src/HelloJava2/HelloJava2.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class HelloJava2 {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Тестовый пример");
-        frame.add(new HelloComponent2("Перемещение по окну"));
+        frame.add(new HelloComponent2("Передвинь =)"));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 300);
         frame.setVisible(true);
