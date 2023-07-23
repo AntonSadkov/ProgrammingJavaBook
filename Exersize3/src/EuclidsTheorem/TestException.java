a444f71f218a96a1e1a73efbf87979b870dc0a60
AntonSadkov <antonsadkov96@gmail.com>
2023-07-24 01:33:40 +0300

    Add EuclidsTheorem, exersize exception handing

---
+++ Exersize3/src/EuclidsTheorem/TestException.java
@@ -0,0 +1,45 @@
+package EuclidsTheorem;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TestException {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+
+        try {
+            Scanner in = new Scanner(System.in);
+            System.out.print("a = ");
+            a = in.nextInt();
+            System.out.print("b = ");
+            b = in.nextInt();
+        } catch (InputMismatchException ime) {
+            System.err.println("Пиши цифры!");
+        }
+
+        if (args.length == 2) {
+            try {
+                a = Integer.parseInt(args[0]);
+                b = Integer.parseInt(args[1]);
+            } catch (NumberFormatException nfe) {
+                System.err.println("""
+                        Arguments were not both numbers.
+                        Using defaults.""");
+            }
+        } else {
+            System.err.println("""
+                    Wrong number of arguments (expected 2).
+                    Using defaults.""");
+        }
+        System.out.println("The GCD of " + a + " and " + b + " is ");
+        while (b != 0) {
+            if (a > b) {
+                a = a -b;
+            } else {
+                b = b - a;
+            }
+        }
+        System.out.println(a);
+    }
+}
